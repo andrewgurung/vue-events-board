ac78cf81a0b1a72d55cc3fb9559944d10f735022
andrewgurung <andrewgurungg@gmail.com>
2017-04-10 20:56:18 -0400

    Init Vue instance

---
+++ app.js
@@ -0,0 +1,14 @@
+new Vue({
+  // Target the div with id of 'events'
+  el: '#events',
+
+  // Here we can register any values/collection that hold data for application
+  data: {},
+
+  // Run when application loads
+  ready: function() {},
+
+  // Register custom methods for the app
+  methods: {}
+
+});
