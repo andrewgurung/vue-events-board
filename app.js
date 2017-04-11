5a67b224cc21c933e5f16b289d4594c87a235a7c
Andrew Gurung <andrewgurung@users.noreply.github.com>
2017-04-11 14:09:39 -0400

    Add default events + implement addEvent method

--- app.js
+++ app.js
@@ -9,12 +9,49 @@ new Vue({
   },
 
   // Run when application loads
-  ready: function() {},
+  ready: function() {
+    // When application loads, call the method that initialized some data
+    this.fetchEvents();
+  },
 
   // Register custom methods for the app
   methods: {
+
+     // We dedicate a method to retrieving and setting some data
+    fetchEvents: function() {
+      var events = [
+        {
+          id: 1,
+          name: 'TIFF',
+          description: 'Toronto International Film Festival',
+          date: '2017-03-11'
+        },
+        {
+          id: 2,
+          name: 'The Martian Premiere',
+          description: 'The Martian comes to theatres.',
+          date: '2015-10-02'
+        },
+        {
+          id: 3,
+          name: 'SXSW',
+          description: 'Music, film and interactive festival in Austin, TX.',
+          date: '2016-03-11'
+        }
+      ];
+
+      // $set is a convenience method provided by Vue that is similar to pushing data into array
+      this.$set('events', events);
+    },
+
+    // Adds an event to the existing events array
     addEvent: function() {
-      console.log('addEvent fired!');
+      if(this.event.name) {
+        this.events.push(this.event);
+
+        // Reset form input fields
+        this.event = {name: '', description: '', date: ''};
+      }
     }
   }
 
